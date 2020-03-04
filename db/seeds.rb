0ef71a58bdc0069af346c6b778033bb931484ec3
waltergrayiii <tripgray@yahoo.com>
2020-03-03 18:50:53 -0600

    Added some products

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+product = Product.new({ name: "Snickers", price: 2, image_url: "https://cdn.shoplightspeed.com/shops/621581/files/11567192/300x250x2/snickers-snickers-bar-1-ct.jpg", description: "Snickers is a brand name chocolate bar made by the American company Mars, Incorporated, consisting of nougat topped with caramel and peanuts that has been enrobed in milk chocolate." })
+product.save
+
+product = Product.new({ name: "Reeses Peanut Butter Cups", price: 3, image_url: "https://d2lnr5mha7bycj.cloudfront.net/product-image/file/large_bf3c7b5c-6e26-4810-bc08-f32435f869f2.jpeg", description: "Reese's Peanut Butter Cups are an American candy consisting of a chocolate cup filled with peanut cream, marketed by The Hershey Company. They were created by H. B. Reese, a former dairy farmer and shipping foreman for Milton S." })
+product.save
+
+product = Product.new({ name: "M&Ms", price: 1, image_url: "https://d2lnr5mha7bycj.cloudfront.net/product-image/file/large_8cee1da3-1f50-4232-83d6-4c5c30f5d1f3.jpeg", description: "M&M's are colorful button-shaped chocolates, each of which has the letter m printed in lower case in white on one side, consisting of a candy shell surrounding a filling which varies depending upon the variety of M&M's. ... M&M's are the flagship product of the Mars Wrigley Confectionery division of Mars, Incorporated." })
+product.save
+
+product = Product.new({ name: "Twix", price: 2, image_url: "https://target.scene7.com/is/image/Target/GUEST_06a0748f-11bd-4426-ae58-ac9561234ad6?wid=488&hei=488&fmt=pjpeg", description: "Twix. Twix is a chocolate bar made by Mars, Inc., consisting of a biscuit applied with other confectionery toppings and coatings (most frequently caramel and milk chocolate). Twix are packaged with two or four bars in a wrapper." })
+product.save
